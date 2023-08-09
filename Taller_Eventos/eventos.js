9fe88723fff1e8b2d4eb1265cfea24edb08fafb7
masteraised <leonxgx@gmail.com>
2023-08-09 18:23:28 -0300

    Update eventos.js

--- Taller_Eventos/eventos.js
+++ Taller_Eventos/eventos.js
@@ -1,5 +1,10 @@
 const divModificar = document.getElementById('divModificar');
+const miBoton = document.getElementById('miBoton');
 
 divModificar.addEventListener('click', function() {
   alert('Hola! Soy el div');
+});
+
+miBoton.addEventListener('click', function() {
+   event.stopPropagation();
 });
