823ebce7169afb3d620f36ba94530a830fb00c93
masteraised <leonxgx@gmail.com>
2023-08-09 14:05:18 -0300

    trabajo finalizado 7.1.3

--- registro tarea 7.3.1/js/validacion.js
+++ registro tarea 7.3.1/js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.getElementById('regBtn').addEventListener('click', function(event) {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+    const terminos = document.getElementById('terminos').checked;
+
+
+
+    if ( nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "" || !terminos) 
+    { showAlertError(); } 
+     else if (password1.length < 6) { showAlertError(); }
+     else if (password1 !== password2) { showAlertError(); } else { showAlertSuccess(); }
+    
+});
+
