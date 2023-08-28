5235ce7e3382d1097d19d5a5b6ece7c1b22dbd92
masteraised <leonxgx@gmail.com>
2023-08-28 20:45:21 -0300

    Update products.js

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -1,33 +1,47 @@
-window.addEventListener("DOMContentLoaded", async () => {
-    const productContainer = document.getElementById("product-list"); // Obtenemos el contenedor donde mostraremos los productos
-    
-    try {
-      // Realizamos la solicitud a la URL que contiene los productos en JSON
-      const response = await fetch("https://japceibal.github.io/emercado-api/cats_products/101.json");
-      // Convertimos la respuesta JSON en un objeto JavaScript
-      const data = await response.json();
+// Funcion para hacer el fetch de la API
+async function fetchProductData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+    return null;
+  }
+}
 
-      // Iteramos a través de los datos y mostramos cada producto en el contenedor
-      data.products.forEach(product => {
-        const productCard = document.createElement("div"); // Creamos un div para cada producto
-        // Aplicamos estilos
-        productCard.className = "list-group-item product-item d-flex justify-content-between align-items-center";
-        // Generamos el contenido html para el producto
-        productCard.innerHTML = `
-          <div class="d-flex align-items-center">
-            <img src="${product.image}"  alt="${product.name}" style="width: 25%;" class="mr-3 product-image">
-            <div style="width:100%;">
-              <h5 style ="margin-left: 5px;" class=" fw-bold display-8 product-title">${product.name}</h5>
-              <p style ="margin-left: 5px;" class="product-price"> ${product.currency} ${product.cost}</p>
-              <p style ="margin-left: 5px;" class="product-description" >${product.description}</p>
-              <p style ="margin-left: 5px;" class=" text-end"> ${product.soldCount} vendidos</p>
-            </div>
-          </div>
-        `;
-        productContainer.appendChild(productCard); // Agregamos el elemento del producto al contenedor principal
-      });
+// Funcion para mostrar el producto en el HTML
+function mostrarProduct(product) {
+  const productCard = document.createElement("div");
+  productCard.className = "list-group-item product-item d-flex justify-content-between align-items-center";
+  productCard.innerHTML = `
+    <div class="d-flex align-items-center">
+      <img src="${product.image}" alt="${product.name}" style="width: 25%;" class="mr-3 product-image">
+      <div style="width:100%;">
+        <h5 style="margin-left: 5px;" class="fw-bold display-8 product-title">${product.name}</h5>
+        <p style="margin-left: 5px;" class="product-price">${product.currency} ${product.cost}</p>
+        <p style="margin-left: 5px;" class="product-description">${product.description}</p>
+        <p style="margin-left: 5px;" class="text-end">${product.soldCount} vendidos</p>
+      </div>
+    </div>
+  `;
+  return productCard;
+}
 
-    } catch (error) {
-      console.error("Error al obtener los productos:", error);
-    }
-  });
+//Funcion principal que se ejecuta al terminar el DOM
+async function main() {
+  const productContainer = document.getElementById("product-list");
+
+  const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+  const data = await fetchProductData(url);
+
+  if (data) {
+    data.products.forEach(product => {
+      const productCard = mostrarProduct(product);
+      productContainer.appendChild(productCard);
+    });
+  }
+}
+
+// Event listener para llamar la funcion principal cuando carga la pagina
+document.addEventListener("DOMContentLoaded", main);
